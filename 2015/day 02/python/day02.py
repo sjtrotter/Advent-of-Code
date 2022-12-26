11d6427fa639414eb6e91e8c1d376e29fbf3f9ff
sjtrotter <stephen.j.trotter@gmail.com>
2022-12-26 14:29:54 -0600

    2015 day2 done.

--- 2015/day 02/python/day02.py
+++ 2015/day 02/python/day02.py
@@ -4,8 +4,13 @@ DEBUG = False
 
 def parse_input(input_data):
     # DEFINE CUSTOM PARSING HERE
-    ...
+    data = [line.strip() for line in input_data.split('\n')]
+    lists = []
+    for line in data:
+        lists.append(line.split('x'))
 
+    return lists
+    
 
 def debug_print(msg):
     # Use ANSI escape codes to set the text color to yellow
@@ -38,7 +43,26 @@ def main():
 
     # CODE REST OF PROBLEM MAINLOOP HERE
     data = parse_input(input_data) # Customize to problem
+    debug_print(data)
 
+    # data: list of l,w,h lists
+    totalPaper = 0
+    totalRibbon = 0
+    for i in data:
+        # debug_print(i)
+        l,w,h = i
+        l,w,h = int(l),int(w),int(h)
+        x,y,z = int(l)*int(w), int(w)*int(h), int(h)*int(l)
+        slack = min(x,y,z)
+        ribbonLength = min(l+w,w+h,l+h)*2
+        bowLength = l*w*h
+        ribbonRequired = ribbonLength + bowLength
+        paperRequired = 2*x + 2*y + 2*z + slack
+        totalPaper += paperRequired
+        totalRibbon += ribbonRequired
+
+    print("part1:",totalPaper)
+    print("part2:",totalRibbon)
 
 
 
