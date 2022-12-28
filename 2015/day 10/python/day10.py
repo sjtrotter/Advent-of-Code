61682c5f5c214d8c9669aa384d383721786c1e86
sjtrotter <stephen.j.trotter@gmail.com>
2022-12-27 21:54:40 -0600

    2015 day 10 done

--- 2015/day 10/python/day10.py
+++ 2015/day 10/python/day10.py
@@ -13,6 +13,22 @@ def debug_print(msg):
     if DEBUG:
         print('\033[33m[+]\033[0m', msg)
 
+def lookAndSay(a):
+    # had this working but it was taking a LOOONG time.
+    # CHAT GPT refined the function to this:
+    result = []
+    i = 0
+    while i < len(a):
+        char = a[i]
+        count = 1
+        for j in range(i+1, len(a)):
+            if a[j] != char:
+                break
+            count += 1
+        result.append(str(count) + char)
+        i += count
+    return ''.join(result)
+
 
 def main():
     global DEBUG
@@ -32,15 +48,27 @@ def main():
     # Read input file if --input flag is set
     if args.input_file:
         with open(args.input_file, 'r') as f:
-            input_data = f.read()
-        debug_print(f'Read input from {args.input_file}: {input_data}')
+            input_data = f.read().strip()
+        debug_print(f'Read input from {args.input_file}: \n{input_data}')
 
 
     # CODE REST OF PROBLEM MAINLOOP HERE
-    data = parse_input(input_data) # Customize to problem
+    # data = parse_input(input_data) # Customize to problem
 
+    input = input_data
+    debug_print(lookAndSay(input_data))
+    # debug_print(lookAndSay("1211"))
+    for i in range(40):
+        input = lookAndSay(input)
+        # debug_print(input)
 
+    print("part1:",len(input))
 
+    input = input_data
+    for i in range(50):
+        input = lookAndSay(input)
+
+    print("part2:",len(input))
 
 if __name__ == '__main__':
     main()
